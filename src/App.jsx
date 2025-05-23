b9e1d35ac0bde3185f5249f0239e4c362d046f61
alexandraoliveira868 <alexandraoliveira1526@gmail.com>
2025-05-23 17:22:33 +0000

    Aula 23/05

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
 function App() {
+  const texts = [{title:"Meu título 1", text: "Meu texto 1"},
+    {title:"Meu título 2", text: "Meu texto 2"},
+    {title:"Meu título 3", text: "Meu texto 3"},
+    {title:"Meu título 4", text: "Meu texto 4"}, 
+    {title:"Meu título 5", text: "Meu texto 5"}];
   return (
-<>
-    <MyText />
-    <MyText />
-    <MyText />
-    </>
-  )
-};
+    //React Fragment
+<>{texts.map((text, index) => {
+    return (
+        <MyText key={index} title={`${text.title} ${index +1}`}>
+          {text.text}
+        </MyText>
+      );
+    })}</>
+    
+  
+  );
+}
 
 export default App;
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,16 +1,12 @@
+import { Children } from 'react';
 import styles from './MyText.module.css';
 
-export function MyText(){
+export function MyText({title, texts, children}) {  
     return (
         <div className={styles.container}>
         <div className={styles.div}>
-            <h1 className={styles.title}>Meu primeiro React App</h1>
-            <p className={styles.text}>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Non a similique id nihil, quaerat repellendus sapiente assumenda 
-                velit blanditiis dolore. Commodi vitae tempora quas excepturi 
-                inventore laboriosam consectetur at aut!
-            </p>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.text}>{children}</p>
         </div>
         </div>
     )

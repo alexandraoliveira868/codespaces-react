1ec9aefe5cde8f3aa8594b6f18099efae6b36c2b
alexandraoliveira868 <alexandraoliveira1526@gmail.com>
2025-06-26 16:54:21 +0000

    Desafio 4 26/06

--- src/components/E2/LuckyNumber.jsx
+++ src/components/E2/LuckyNumber.jsx
@@ -3,14 +3,23 @@ import styles from './LuckyNumber.module.css';
 
 export function LuckyNumber() {
  const [luckyNumber, setLuckyNumber ] = useState(0);
+ const [history, setHistory] = useState([]);
+ const [warning, setWarning] = useState("");
 
  function handleClick() {
-        setLuckyNumber(Math.floor(Math.random() * 40) + 1);
+        const drawnNumber = Math.floor(Math.random() * 40) + 1;
+        if (history.includes(drawnNumber)) {
+            setWarning("Já foi sorteado esse número!");
+            setLuckyNumber(drawnNumber);
+            return; }
+        setHistory([...history, drawnNumber]);
+        setLuckyNumber(drawnNumber);
+        setWarning("");
     }
     return (
         <div className={styles.container}>
             {luckyNumber === 0 ? (
-                 <h1>Lucky Number </h1>
+                 <h1>Lucky Number 🎲</h1>
      ) : (
                 <h1>Lucky Number = {luckyNumber}</h1>
     )
@@ -20,6 +29,20 @@ export function LuckyNumber() {
             <button className={styles.button} onClick={handleClick}>
                 I'm Feeling Lucky Today!
                 </button>
+
+            {warning && <p className={styles.warning}>{warning}</p>}
+            {history.length > 0 && (
+                <>
+                <h2 className={styles.subtitle}>Números Sorteados: </h2>
+                <ul className={styles.list}>
+                    {history.map((num) => (
+        <li key={num}>
+            {num}
+        </li>
+    ))}
+</ul>
+                </>
+            )}
         </div>
     );
 }
